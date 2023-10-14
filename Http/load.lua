6548a04c1512e73b9d0eaee653401519e98f6811
netheround <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-10-14 15:12:10 +0300

    Update load.lua

--- Http/load.lua
+++ Http/load.lua
@@ -1,6 +1,6 @@
 local HttpService = game:GetService("HttpService")
 
-return function(url: string)
+return function(url: string) -- Taks an argument as "string" and it extracts the data from the url and then it decodes the json format to lua table and returns the data.
 	if not url then return error("Argument 1 (\"url: string\") missing or nil") end
 	assert(type(url) == "string", "Argument 1 (\"url: string\") must be a string, got "..tostring(type(url))..".")
 	
