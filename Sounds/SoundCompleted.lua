57bad80b0a0ca896512daca90a85ff5355455d1f
netheround <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-10-14 15:13:44 +0300

    Update SoundCompleted.lua

--- Sounds/SoundCompleted.lua
+++ Sounds/SoundCompleted.lua
@@ -1,4 +1,4 @@
-return function(Sound: Sound, Callback: () -> ())
+return function(Sound: Sound, Callback: () -> ()) -- When the sound played you can create a function same as Tween.Completed
 	task.wait(Sound.TimeLength)
 	Callback()
 end
