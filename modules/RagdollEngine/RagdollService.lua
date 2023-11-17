7d7353acb2754a4d4d09e831ccbb8a5f80fdd7ac
netheround <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-11-18 01:16:53 +0200

    Update RagdollService.lua

--- modules/RagdollEngine/RagdollService.lua
+++ modules/RagdollEngine/RagdollService.lua
@@ -1,33 +1,31 @@
 --!strict
 
 --[[
-
-	> NOTE: This module uses JumpPower, not JumpHeight.
-
 	Name: RagdollService.lua
 	Author: netheround
 	Description: Nice ragdolling module that works with both R6/R15
 	
 	License: MIT
-	Version: RagdollService 1.2
+	Version: RagdollService 1.3
 	
 	API Documentation: {
 	
 		1. RagdollCharacter()
 		
-		-> Ragdolls the given characters + changes the WalkSpeed/JumpPower to 0.
+		-> Ragdolls the given characters + changes the WalkSpeed/JumpPower or JumpHeight to 0.
+		
 		
 		Parameters:
 		[Character: Model]
 		
 		2. UnragdollCharacter()
 		
-		-> Unragdolls the given characters + changes the WalkSpeed/JumpPower to the given ones or default which is 16/50.
+		-> Unragdolls the given characters + changes the WalkSpeed/JumpPower or JumpHeight to the given ones or default which is 16/50 or 7.2.
 		
 		Parameters:
 		[Character: Model]
 		[WalkSpeed: number]
-		[JumpPower: number]
+		[JumpPowerOrJumpHeight: number]
 		
 		3. IsRagdolled()
 		
@@ -42,7 +40,7 @@
 
 ]]--
 
-local RagdollService = { _VERSION = "RagdollService 1.2" }
+local RagdollService = { _VERSION = "RagdollService 1.3" }
 
 function RagdollService:RagdollCharacter(Character): ()
 	local Humanoid: Humanoid? = Character:FindFirstChild("Humanoid")
@@ -73,14 +71,19 @@ function RagdollService:RagdollCharacter(Character): ()
 		Humanoid:ChangeState(Enum.HumanoidStateType.PlatformStanding)
 
 		Humanoid.WalkSpeed = 0
-		Humanoid.JumpPower = 0
+		
+		if Humanoid.UseJumpPower then
+			Humanoid.JumpPower = 0
+		else
+			Humanoid.JumpHeight = 0
+		end
 
 		Humanoid.PlatformStand = true
 		Humanoid.AutoRotate = false
 	end
 end
 
-function RagdollService:UnragdollCharacter(Character, WalkSpeed: number?, JumpPower: number?): ()
+function RagdollService:UnragdollCharacter(Character, WalkSpeed: number?, JumpPowerOrJumpHeight: number?): ()
 	local Humanoid: Humanoid? = Character:WaitForChild("Humanoid")
 
 	if Humanoid then
@@ -99,7 +102,11 @@ function RagdollService:UnragdollCharacter(Character, WalkSpeed: number?, JumpPo
 		Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp)
 
 		Humanoid.WalkSpeed = WalkSpeed or 16
-		Humanoid.JumpPower = JumpPower or 50
+		if Humanoid.UseJumpPower then
+			Humanoid.JumpPower = JumpPowerOrJumpHeight or 50
+		else
+			Humanoid.JumpHeight = JumpPowerOrJumpHeight or 7.2
+		end
 
 		Humanoid.AutoRotate = true
 	end
