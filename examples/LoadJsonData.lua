ad6d46959010e8639101824c81b8bfa12c27193e
netheround <115834356+fsdfdfgdfghSD@users.noreply.github.com>
2023-11-19 02:14:03 +0200

    Update LoadJsonData.lua

--- examples/LoadJsonData.lua
+++ examples/LoadJsonData.lua
@@ -1,5 +1,5 @@
 local LoadJsonData = require("pathToLoadJsonData")
 
-local Data = LoadJsonData("https://catfact.ninja/fact") -- Retrieves the information from the web and decoding into lua table format
+local Data = LoadJsonData("https://catfact.ninja/fact") -- Retrieves the information from the web and decoding json to lua table format.
 
 print(Data.fact) -- the api returns fact and length, we're printing fact.
